899efd33034fdf173b57b95419b55385c484bdd0
Syrym <suleimenov.syrym.s@gmail.com>
2026-06-08 13:17:02 +0500

    ES Module fix require -> import

--- deploy.js
+++ deploy.js
@@ -1,24 +1,31 @@
-require('dotenv').config({ path: '.env.ftp' });
+import dotenv from 'dotenv';
+import FtpDeploy from 'ftp-deploy';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const FtpDeploy = require('ftp-deploy');
+dotenv.config({ path: '.env.ftp' });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-    user: process.env.FTP_USER,
-    password: process.env.FTP_PASSWORD,
-    host: process.env.FTP_HOST,
-    port: parseInt(process.env.FTP_PORT || '21', 10),
-    localRoot: __dirname + '/dist',
-    remoteRoot: process.env.FTP_REMOTE_ROOT,
-    include: ['*', '**/*'],
-    deleteRemote: false,
+   user: process.env.FTP_USER,
+   password: process.env.FTP_PASSWORD,
+   host: process.env.FTP_HOST,
+   port: parseInt(process.env.FTP_PORT || '21', 10),
+   localRoot: __dirname + '/dist',
+   remoteRoot: process.env.FTP_REMOTE_ROOT,
+   include: ['*', '**/*'],
+   deleteRemote: false,
 };
 
-ftpDeploy.deploy(config)
-    .then(() => {
-        console.log('Upload complete');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+ftpDeploy
+   .deploy(config)
+   .then(() => {
+      console.log('Upload complete');
+   })
+   .catch((err) => {
+      console.error(err);
+   });
